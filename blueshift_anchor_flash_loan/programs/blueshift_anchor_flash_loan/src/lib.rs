def0d7623532d9b16a6acc3ba8146d865132a398
Dipendra Singh <dipendras953@gmail.com>
2025-08-27 16:57:51 +0530

    feat(blueshift_anchor_flash_loan): add borrow instruction

--- blueshift_anchor_flash_loan/programs/blueshift_anchor_flash_loan/src/lib.rs
+++ blueshift_anchor_flash_loan/programs/blueshift_anchor_flash_loan/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::{
     solana_program::sysvar::instructions::{
-        load_instruction_at_checked, ID as INSTRUCTIONS_SYSVAR_ID,
+        load_current_index_checked, load_instruction_at_checked, ID as INSTRUCTIONS_SYSVAR_ID,
     },
     Discriminator,
 };
@@ -17,6 +17,61 @@ pub mod blueshift_anchor_flash_loan {
     use super::*;
 
     pub fn borrow(ctx: Context<Loan>, borrow_amount: u64) -> Result<()> {
+        require!(borrow_amount > 0, ProtocolError::InvalidAmount);
+
+        let seeds = &[b"protocol".as_ref(), &[ctx.bumps.protocol]];
+        let signer_seeds = &[&seeds[..]];
+
+        transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.protocol_ata.to_account_info(),
+                    to: ctx.accounts.borrower_ata.to_account_info(),
+                    authority: ctx.accounts.protocol.to_account_info(),
+                },
+                signer_seeds,
+            ),
+            borrow_amount,
+        )?;
+
+        let ixs = ctx.accounts.instructions.to_account_info();
+
+        let current_index = load_current_index_checked(&ixs)?;
+        require_eq!(current_index, 0, ProtocolError::InvalidIx);
+
+        let instuction_sysvar = ixs.try_borrow_data()?;
+        let len = u16::from_le_bytes(instuction_sysvar[0..2].try_into().unwrap());
+
+        if let Ok(repay_ix) = load_instruction_at_checked(len as usize - 1, &ixs) {
+            require_keys_eq!(repay_ix.program_id, ID, ProtocolError::InvalidProgram);
+            require!(
+                repay_ix.data[0..8].eq(instruction::Repay::DISCRIMINATOR),
+                ProtocolError::InvalidIx
+            );
+
+            require_keys_eq!(
+                repay_ix
+                    .accounts
+                    .get(3)
+                    .ok_or(ProtocolError::InvalidBorrowerAta)?
+                    .pubkey,
+                ctx.accounts.borrower_ata.key(),
+                ProtocolError::InvalidBorrowerAta
+            );
+
+            require_keys_eq!(
+                repay_ix
+                    .accounts
+                    .get(4)
+                    .ok_or(ProtocolError::InvalidProtocolAta)?
+                    .pubkey,
+                ctx.accounts.protocol_ata.key(),
+                ProtocolError::InvalidProtocolAta
+            );
+        } else {
+            return Err(ProtocolError::MissingRepayIx.into());
+        }
         Ok(())
     }
 

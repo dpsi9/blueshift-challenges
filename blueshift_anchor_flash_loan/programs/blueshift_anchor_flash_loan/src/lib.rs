1549cfe0ba1e7da4585df147af845109d0523f2a
Dipendra Singh <dipendras953@gmail.com>
2025-08-27 16:08:15 +0530

    feat(blueshift_anchor_flash_loan): add states and error

--- blueshift_anchor_flash_loan/programs/blueshift_anchor_flash_loan/src/lib.rs
+++ blueshift_anchor_flash_loan/programs/blueshift_anchor_flash_loan/src/lib.rs
@@ -1,16 +1,84 @@
 use anchor_lang::prelude::*;
+use anchor_lang::{
+    solana_program::sysvar::instructions::{
+        load_instruction_at_checked, ID as INSTRUCTIONS_SYSVAR_ID,
+    },
+    Discriminator,
+};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{transfer, Mint, Token, TokenAccount, Transfer},
+};
 
-declare_id!("Cq1EBT9b1yejknH4dLfLuaJ8X6db1KkstpTM3tP5hCDP");
+declare_id!("22222222222222222222222222222222222222222222");
 
 #[program]
 pub mod blueshift_anchor_flash_loan {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn borrow(ctx: Context<Loan>, borrow_amount: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn repay(ctx: Context<Loan>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Loan<'info> {
+    #[account(mut)]
+    pub borrower: Signer<'info>,
+    #[account(
+        seeds = [b"protocol".as_ref()],
+        bump
+    )]
+    pub protocol: SystemAccount<'info>,
+
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = borrower,
+        associated_token::mint = mint,
+        associated_token::authority = borrower
+    )]
+    pub borrower_ata: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = protocol
+    )]
+    pub protocol_ata: Account<'info, TokenAccount>,
+
+    #[account(address = INSTRUCTIONS_SYSVAR_ID)]
+    instructions: UncheckedAccount<'info>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum ProtocolError {
+    #[msg("Invalid instruction")]
+    InvalidIx,
+    #[msg("Invalid instruction index")]
+    InvalidInstructionIndex,
+    #[msg("Invalid amount")]
+    InvalidAmount,
+    #[msg("Not enough funds")]
+    NotEnoughFunds,
+    #[msg("Program mismatch")]
+    ProgramMismatch,
+    #[msg("Invalid program")]
+    InvalidProgram,
+    #[msg("Invalid borrower ata")]
+    InvalidBorrowerAta,
+    #[msg("Invalid protocol ata")]
+    InvalidProtocolAta,
+    #[msg("Missing repay instruction")]
+    MissingRepayIx,
+    #[msg("Missing borrow instuction")]
+    MissingBorrowIx,
+    #[msg("Overflow")]
+    Overflow,
+}
